ff0edf0bd64f8bd126d2c71a2109c9875db0e284
seantarogers <seantarogers@yahoo.co.uk>
2016-03-10 22:55:31 +0000

    remove old comments

--- Ui/js/main.js
+++ Ui/js/main.js
@@ -15,30 +15,4 @@ render(
     <Route path='/1' component={CustomerController1} onEnter={_customerEnter(1)} />         
     <Route path='/2' component={CustomerController2} onEnter={_customerEnter(2)} />         
     </Router>, document.getElementById('app'));
-   //<Route path='/policies:customerName' component={PoliciesController} />
-
-
-
-
-//what are the main features of a controller?
-
-//set up listen to store
-//remove listen to store
-//handle events -- on some object?
-// convert state to props --
-// render and render smaller bits - smaller bits pushed out to functions
-
-
-//dont like base controllers which call things defined on extension
-//dont like super.
-
-
-//eaasy to share utilities but how about
-//Lifecycle Hooks and State Providers
-
-
-//previously you could use a mixin the declare add listenrs, remove listeners, on change and set state
-//we are now use inheritence to do this which is no better as it has the implicit contracts of ovveridden methods etc
-//pass in a list of stores to a mixin ...stores and do common operations on it
-
-//A higher-order component is just a function that takes an existing component and returns another component that wraps it.
+ 
